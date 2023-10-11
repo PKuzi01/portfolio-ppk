6d56929465d3aefde396f7a1fcee90e1e87008d6
Philisizwe Kuzwayo <cre8storeese@gmail.com>
2023-10-11 16:33:25 +0200

    Hero structure

---
+++ src/Hero.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import './Hero.css';
+
+function Hero() {
+    return (
+        <div className="hero">
+            <h2>
+                Hello.
+                <br />
+                My name is
+            </h2>
+            <h1>Philisizwe Kuzwayo</h1>
+            <h2>
+                You can call me Phila (Pee-la).
+                <br />
+                Nice to meet you.
+            </h2>
+        </div>
+    )
+}
+
+export default Hero
