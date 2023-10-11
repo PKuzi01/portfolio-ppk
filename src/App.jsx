133c38cc34d37b83e0afe2367775c7655207918b
Philisizwe Kuzwayo <cre8storeese@gmail.com>
2023-10-11 17:59:44 +0200

    Added portfolio structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import Hero from "./Hero";
 import About from "./About";
+import Portfolio from "./Portfolio";
 
 
 function App() {
   return (
-    <div className='container'>
+    <div className='container-fluid'>
       <Hero />
       <About />
+      <Portfolio />
     </div>
 
   )
---
+++ src/Portfolio.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+function Portfolio() {
+    return (
+        <div className="portfolio">
+            <h2>Wanna see my work?</h2>
+            <div className="row">
+                <div className="col-6">
+                    <h3>Rock Paper Scissors Game</h3>
+                    <p>Inspired by a lesson in the Foundations course on the Odin Project</p>
+                    <a class="portfolio-link" href="https://pkuzi01.github.io/rock-paper-scissors-ppk/" target="_blank">See project here</a> 
+                </div>
+                <div className="col-6">
+                    <h3>Weather App</h3>
+                    <p>Completed as part of the SheCodes Plus workshop.</p>
+                    <a class="portfolio-link" href="https://sc-react-weather-app-ppk.netlify.app/" target="_blank">See project here</a>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-6">
+                    <h3>Practice Portfolio</h3>
+                    <p>Completed as part of the SheCodes Responsive workshop.</p>
+                    <a class="portfolio-link" href="https://pkuzi01.github.io/sc-responsive-practice-portfolio-ppk/" target="_blank">See project here</a>
+                </div>
+                <div className="col-6">
+                    <h3>Music Fan Page</h3>
+                    <p>Completed as part of the SheCodes Responsive Add-On workshop.</p>
+                    <a class="portfolio-link" href="https://the-okonma-project-ppk.netlify.app/" target="_blank">See project here</a> 
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Portfolio;
