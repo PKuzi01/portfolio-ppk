2a2f62703df47037b102f3d015470c7892ccf84f
Philisizwe Kuzwayo <cre8storeese@gmail.com>
2023-10-14 17:49:41 +0200

    Contact styling added

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Contact.css"
 
 function Contact() {
     return (
@@ -6,10 +7,10 @@ function Contact() {
             <h2>Wanna hang out? You can find me here:</h2>
             <ul>
                 <li><a href="https://github.com/PKuzi01" target="_blank"><strong>Github</strong></a></li>
-                <li><a href="https://www.linkedin.com/in/philisizwe-kuzwayo-3116541a8/" target="_blank"><strong>LinkedIn</strong></a></li>
+                <li><a href="https://www.linkedin.com/in/philisizwekuzwayo/" target="_blank"><strong>LinkedIn</strong></a></li>
                 <li><a href="mailto:ilaphi@outlook.com" target="_blank"><strong>Email</strong></a></li>
             </ul>
-            <h3>Hope to hear from you soon.</h3>
+            <h2>Hope to hear from you soon.</h2>
             <div class="backToTop">
                 <a href="#hero">Back to top</a>
             </div>
