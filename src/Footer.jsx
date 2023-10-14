f75c06fc6d97afff9782bb05a8cf5e0c49689530
Philisizwe Kuzwayo <cre8storeese@gmail.com>
2023-10-14 13:21:21 +0200

    Added Footer structure

---
+++ src/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+    return (
+        <div className="footer">
+            <footer>
+                Built by yours truly, Philisizwe Kuzwayo.
+            </footer>
+        </div>
+    )
+}
+
+export default Footer;
